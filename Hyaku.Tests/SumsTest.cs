a34be764e70fc599e179429ba068217c47836cfe
Nate Grigg <hyaku@nategrigg.com>
2011-05-30 22:44:07 +0600

    added a test project - but that's all, didn't write any tests

---
+++ Hyaku.Tests/SumsTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Silverlight.Testing;
+
+namespace Hyaku.Tests
+{
+    [TestClass]
+    public class SumsTest : SilverlightTest
+    {
+        [TestMethod]
+        public void AlwaysPass()
+        {
+            Assert.IsTrue(true, "this method always passes");
+        }
+
+        [TestMethod]
+        public void AlwaysFail()
+        {
+            Assert.IsFalse(true, "this method always fails");
+        }
+    }
+}
